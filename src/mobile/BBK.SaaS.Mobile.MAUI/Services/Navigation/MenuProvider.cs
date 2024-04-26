2dc01222934bac6d74b10edc1bf3fd7ae3364cb3
Nguyễn Thanh Long <0868652542long@gmail.com>
2024-04-26 09:20:10 +0700

    Revert "Merge branch 'dev' into ntlong2"
    This reverts commit 335c5840e12b468a46e6440fb2a00576e2e367c6, reversing
    changes made to d900d6d8f68858834a5a0b9b8cbacb82c2d0a901.

--- src/mobile/BBK.SaaS.Mobile.MAUI/Services/Navigation/MenuProvider.cs
+++ src/mobile/BBK.SaaS.Mobile.MAUI/Services/Navigation/MenuProvider.cs
@@ -7,7 +7,6 @@ using BBK.SaaS.Localization;
 using BBK.SaaS.Models.NavigationMenu;
 using BBK.SaaS.Services.Account;
 using BBK.SaaS.Services.Permission;
-using Microsoft.Maui.Controls.Maps;
 
 namespace BBK.SaaS.Services.Navigation
 {
@@ -210,6 +209,27 @@ namespace BBK.SaaS.Services.Navigation
 
 
                 #endregion 
+
+                        // new NavigationMenuItem
+                        //{
+                        //       Title = L.Localize("Đăng xuất"),
+                        //       Icon = "fa fa-sign-out",
+
+                        //       NavigationUrl  = NavigationUrlConsts.TrangChu
+
+                        //},
+
+
+
+                        /*This is a sample menu item to guide how to add a new item.
+                            ,new NavigationMenuItem
+                            {
+                                Title = "Sample View",
+                                Icon = "MyIcon.png",
+                                TargetType = typeof(_SampleView),
+                                Order = 10
+                            }
+                        */
                     };
                 }
                 else
@@ -217,56 +237,350 @@ namespace BBK.SaaS.Services.Navigation
 
                     #region Người lao động
                     _menuItems = new List<NavigationMenuItem>
+            {
+               
+                  //---------------------------------------------
+                //new NavigationMenuItem
+                //{
+                //    Title = L.Localize("Trang chủ"),
+                //    Icon = "fa fa-home",
+                //    NavigationUrl  = NavigationUrlConsts.TrangChu
+                //},
+                //new NavigationMenuItem
+                //{
+                //    Title = L.Localize("Việc tìm người"),
+                //    Icon = "fas fa-briefcase",
+                //    NavigationUrl  = NavigationUrlConsts.ViecTimNguoi
+                //},
+                //new NavigationMenuItem
+                //{
+                //    Title = L.Localize("Người tìm việc"),
+                //    Icon = "fa fa-user",
+                //    NavigationUrl  = NavigationUrlConsts.NguoiTimViec
+                //},
+
+                ////-Chuyên mục-------------------------------------------
+                // new NavigationMenuItem
+                // {
+                //    Title = L.Localize("Chuyên mục"),
+                //    Items = new List<NavigationMenuItem>
+                //    {
+                //        new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Tin tức"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.TinTuc
+                //        },
+                //        // new NavigationMenuItem
+                //        //{
+                //        //    Title = L.Localize("detail Tin tức"),
+                //        //    //Icon = "fa fa-heart",
+                //        //    NavigationUrl  = NavigationUrlConsts.ArticleDetail
+                //        //},
+                //        new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Thông tin du học"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.ThongTinDuHoc
+                //        },
+                //        new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Thông tin xuất khẩu lao động"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.ThongTinXuatKhauLaoDong
+                //        },
+                //        new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Đào tạo kĩ năng"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.DaotaoKyNang
+                //        },
+                //        new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Đào tạo nghề"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.DaoTaoNghe
+                //        },
+                //        new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Dịch vụ khác"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.DichVuKhac
+                //        },
+                //          new NavigationMenuItem
+                //        {
+                //            Title = L.Localize("Văn bản mới"),
+                //            //Icon = "fa fa-heart",
+                //            NavigationUrl  = NavigationUrlConsts.VanBanMoi
+                //        },
+                //    }
+
+
+                //},
+
+                ////--lien he---
+                // new NavigationMenuItem
+                // {
+                //        Title = L.Localize("Liên hệ & Hỏi đáp"),
+                //        Icon = "fa fa-phone",
+                //        NavigationUrl  = NavigationUrlConsts.LienHe
+                // },
+                // //  new NavigationMenuItem
+                // //{
+                // //       Title = L.Localize("Phiên giao dịch"),
+                // //       Icon = "fab fa-ideal",
+                // //       NavigationUrl  = NavigationUrlConsts.PhienGiaoDich
+                // //},
+                // new NavigationMenuItem
+                // {
+                //        Title = L.Localize("Giới thiệu"),
+                //        Icon = "fa fa-info-circle",
+                //        NavigationUrl  = NavigationUrlConsts.GioiThieu
+                // },
+
+
+                // new NavigationMenuItem
+                // {
+                //    Title = L.Localize("Tenants"),
+                //    Icon = "fa-solid fa-list",
+                //    NavigationUrl = NavigationUrlConsts.Tenants,
+                //    RequiredPermissionName = AppPermissions.Pages_Tenants,
+                // },
+                // new NavigationMenuItem
+                // {
+                //    Title = L.Localize("Users"),
+                //    Icon = "fa-solid fa-filter",
+                //    NavigationUrl= NavigationUrlConsts.User,
+                //    RequiredPermissionName = AppPermissions.Pages_Administration_Users,
+                // },
+
+                 new NavigationMenuItem
+                 {
+                    Title = L.Localize("Hồ sơ cá nhân"),
+                    Items = new List<NavigationMenuItem>
                     {
                         new NavigationMenuItem
                         {
-                           Title = L.Localize("HỒ SƠ CÁ NHÂN"),
-                           Items =  new List<NavigationMenuItem> {
-                                new NavigationMenuItem
-                                {
-                                    Title = L.Localize("Thông tin cá nhân"),
-                                    Icon = "fa font-14 fa-user-circle-o bg-blue-dark rounded-s",
-                                    NavigationUrl =  NavigationUrlConsts.InforNLD,
-                                },
-                                new NavigationMenuItem
-                                {
-                                    Title = L.Localize("Hồ sơ ứng tuyển"),
-                                    Icon = "fa font-14 fa-address-book bg-yellow-dark rounded-s",
-                                    NavigationUrl  = NavigationUrlConsts.BaiUngTuyen,
-                                },
-                                new NavigationMenuItem
-                                {
-                                    Title = L.Localize("Công việc ứng tuyển"),
-                                    Icon = "fa font-14 fa-briefcase bg-green-dark rounded-s",
-                                    NavigationUrl  = NavigationUrlConsts.DSCVDaUngTuyen,
-                                },
-                                new NavigationMenuItem
-                                {
-                                    Title = L.Localize("Danh sách giới thiệu"),
-                                    //Icon = " "
-                                    NavigationUrl  = NavigationUrlConsts.DSGT,
-                                },
-                                new NavigationMenuItem
-                                {
-                                    Title = L.Localize("Danh sách lịch hẹn"),
-                                    NavigationUrl  = NavigationUrlConsts.DanhSachLH,
-                                },
-                                new NavigationMenuItem
-                                {
-                                    Title = L.Localize("Danh sách câu hỏi"),
-                                    NavigationUrl  = NavigationUrlConsts.DanhSachCauHoi,
-                                },
+                                Title = L.Localize("Thông tin cá nhân "),
+                                //Icon = "fa fa-user-circle",
+                                NavigationUrl  = NavigationUrlConsts.InforNLD,
+                        },
+                        new NavigationMenuItem
+                        {
+                                Title = L.Localize("Danh sách hồ sơ ứng tuyển của tôi"),
+                                //Icon = "fa fa-list-alt",
+                                NavigationUrl  = NavigationUrlConsts.BaiUngTuyen,
+                                 //RequiredPermissionName = AppPermissions.,
+                        },
+                         new NavigationMenuItem
+                        {
+                                Title = L.Localize("Danh sách công việc đã ứng tuyển của tôi"),
+                                //Icon = "fa-solid fa-cog",
+                                NavigationUrl  = NavigationUrlConsts.DSCVDaUngTuyen,
+                                 //RequiredPermissionName = AppPermissions.,
+                        },
+                              new NavigationMenuItem
+                        {
+                                Title = L.Localize("Danh sách giới thiệu của tôi"),
+                                //Icon = "fa-solid fa-cog",
+                                NavigationUrl  = NavigationUrlConsts.DSGT,
+                                 //RequiredPermissionName = AppPermissions.,
+                        },
+                          new NavigationMenuItem
+                        {
+                                Title = L.Localize("Danh sách lịch hẹn"),
+                                //Icon = "fa-solid fa-cog",
+                                NavigationUrl  = NavigationUrlConsts.DanhSachLH,
+                                 //RequiredPermissionName = AppPermissions.,
+                        },
+                         new NavigationMenuItem
+                        {
+                                Title = L.Localize("Danh sách câu hỏi của tôi"),
+                                //Icon = "fa-solid fa-cog",
+                                NavigationUrl  = NavigationUrlConsts.DanhSachCauHoi,
+                                 //RequiredPermissionName = AppPermissions.,
+                        },
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Cài đặt"),
+                            //Icon = "fa-solid fa-cog",
+                            NavigationUrl  = NavigationUrlConsts.Settings,
+                        },
+                    }
+ }
 
-                           },
-                        }
 
-                    };
 
+
+                 // new NavigationMenuItem
+                 //{
+                 //       Title = L.Localize("Đăng xuất"),
+                 //       Icon = "fa fa-sign-out",
+
+                 //       //NavigationUrl  = NavigationUrlConsts.TrangChu,
+                       
+                 //       ClickAction = LogOutAndNavToHome,
+                 //},
+                   
+           
+           
+                /*This is a sample menu item to guide how to add a new item.
+                    ,new NavigationMenuItem
+                    {
+                        Title = "Sample View",
+                        Icon = "MyIcon.png",
+                        TargetType = typeof(_SampleView),
+                        Order = 10
+                    }
+                */
+            };
+                    #endregion
                 }
 
-                #endregion
+            }
+            else
+            {
+                _menuItems = new List<NavigationMenuItem>
+            {
+               
+                  //---------------------------------------------
+                new NavigationMenuItem
+                {
+                    Title = L.Localize("Trang chủ"),
+                    Icon = "fa fa-home",
+                    NavigationUrl  = NavigationUrlConsts.TrangChu
+                },
+                new NavigationMenuItem
+                {
+                    Title = L.Localize("Việc tìm người"),
+                    Icon = "fas fa-briefcase",
+                    NavigationUrl  = NavigationUrlConsts.ViecTimNguoi
+                },
+                new NavigationMenuItem
+                {
+                    Title = L.Localize("Người tìm việc"),
+                    Icon = "fa fa-user",
+                    NavigationUrl  = NavigationUrlConsts.NguoiTimViec
+                },
+
+                //-Chuyên mục-------------------------------------------
+                 new NavigationMenuItem
+                 {
+                    Title = L.Localize("Chuyên mục"),
+                    Items = new List<NavigationMenuItem>
+                    {
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Tin tức"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.TinTuc
+                        },
+                        // new NavigationMenuItem
+                        //{
+                        //    Title = L.Localize("detail Tin tức"),
+                        //    //Icon = "fa fa-heart",
+                        //    NavigationUrl  = NavigationUrlConsts.ArticleDetail
+                        //},
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Thông tin du học"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.ThongTinDuHoc
+                        },
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Thông tin xuất khẩu lao động"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.ThongTinXuatKhauLaoDong
+                        },
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Đào tạo kĩ năng"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.DaotaoKyNang
+                        },
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Đào tạo nghề"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.DaoTaoNghe
+                        },
+                        new NavigationMenuItem
+                        {
+                            Title = L.Localize("Dịch vụ khác"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.DichVuKhac
+                        },
+                          new NavigationMenuItem
+                        {
+                            Title = L.Localize("Văn bản mới"),
+                            //Icon = "fa fa-heart",
+                            NavigationUrl  = NavigationUrlConsts.VanBanMoi
+                        },
+                    }
+
+
+                },
+
+                //--lien he---
+                 new NavigationMenuItem
+                 {
+                        Title = L.Localize("Liên hệ & Hỏi đáp"),
+                        Icon = "fa fa-phone",
+                        NavigationUrl  = NavigationUrlConsts.LienHe
+                 },
+                 //  new NavigationMenuItem
+                 //{
+                 //       Title = L.Localize("Phiên giao dịch"),
+                 //       Icon = "fab fa-ideal",
+                 //       NavigationUrl  = NavigationUrlConsts.PhienGiaoDich
+                 //},
+                 new NavigationMenuItem
+                 {
+                        Title = L.Localize("Giới thiệu"),
+                        Icon = "fa fa-info-circle",
+                        NavigationUrl  = NavigationUrlConsts.GioiThieu
+                 },
+
+
+                 new NavigationMenuItem
+                 {
+                    Title = L.Localize("Tenants"),
+                    Icon = "fa-solid fa-list",
+                    NavigationUrl = NavigationUrlConsts.Tenants,
+                    RequiredPermissionName = AppPermissions.Pages_Tenants,
+                 },
+                 new NavigationMenuItem
+                 {
+                    Title = L.Localize("Users"),
+                    Icon = "fa-solid fa-filter",
+                    NavigationUrl= NavigationUrlConsts.User,
+                    RequiredPermissionName = AppPermissions.Pages_Administration_Users,
+                 },
+                  new NavigationMenuItem
+                 {
+                        Title = L.Localize("Đăng nhập"),
+                        Icon = "fa fa-sign-in",
+                        NavigationUrl  = NavigationUrlConsts.Login
+
+
+                 },
+                   
+           
+           
+                /*This is a sample menu item to guide how to add a new item.
+                    ,new NavigationMenuItem
+                    {
+                        Title = "Sample View",
+                        Icon = "MyIcon.png",
+                        TargetType = typeof(_SampleView),
+                        Order = 10
+                    }
+                */
+            };
 
             }
+
         }
 
         //private void LogOutAndNavToHome()
